035272d09afa67c3c7319c98464ac188917a995b
Evan Brass <brassevan@gmail.com>
2022-08-29 15:37:27 -0700

    Fixup html function and remove generics
    This switches to the new paths stuff.
    It also removes the ability to generalize the HTML function using a custom apply expression function.  Personally I never used a custom function, and I think it makes sense to remove that functionality.  A user could always have forked the library anyway.

--- src/templating/html.mjs
+++ src/templating/html.mjs
@@ -3,27 +3,24 @@ import get_or_set from '../lib/get-or-set.mjs';
 import { apply_expression } from './apply-expression.mjs';
 import { descend_paths } from './descendant-path.mjs';
 
-// Cache: strings -> template element
+// Cache: strings -> (HTMLTemplateElement, Paths)
 const template_cache = new WeakMap();
 
-export function make_html(apply_e = apply_expression) {
-	return function html(strings, ...expressions) {
-		// Get the template element:
-		const { template, paths } = get_or_set(template_cache, strings, () => create_template(strings));
+export function html(strings, ...expressions) {
+	// Get the template element:
+	const { template, paths } = get_or_set(template_cache, strings, () => create_template(strings));
 
-		// Instantiate our template:
-		const fragment = document.importNode(template.content, true);
+	// Instantiate our template:
+	const fragment = document.importNode(template.content, true);
 
-		// Apply our expressions:
-		const parts = [...descend_paths(paths, fragment)];
-		if (expressions.length !== parts.length) {
-			throw new Error("A different Number of expressions from parts was found while instantiating: This usually means an html syntax error in the template literal, but can also mean a problem caused by permitted content.");
-		}
-		for (let i = 0; i < expressions.length; ++i) {
-			apply_e(expressions[i], parts[i]);
-		}
-		return fragment;
+	// Retreive the parts using the paths:
+	const parts = descend_paths(paths, fragment);
+
+	if (expressions.length !== parts.length) throw new Error("Template didn't expects a different number of expressions: Usually an HTML syntax error. Check the permitted content rules.");
+
+	// Apply the expressions to the parts:
+	for (let i = 0; i < expressions.length; ++i) {
+		apply_expression(expressions[i], parts[i]);
 	}
+	return fragment;
 }
-
-export default make_html();
