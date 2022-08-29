b5e4d47aa49a4e64a50a1ce48dc2d2fc6c335dce
Evan Brass <brassevan@gmail.com>
2022-08-29 16:15:44 -0700

    Reactivity Init
    This is almost identical to the xstate style reactivity that I've used many many times already.
    The only difference I've made this time around is trying to be small enough to fit in a single file.

---
+++ src/reactivity/reactivity.mjs
@@ -0,0 +1,49 @@
+let context;
+
+export function x(func) {
+	const prev_context = context;
+	func();
+	context = prev_context;
+}
+
+let update_set;
+function propagate_changes() {
+	const updates = update_set;
+	update_set = undefined;
+
+	for (const update of updates) {
+		x(update);
+	}
+}
+export function queue_propagation() {
+	if (!update_set) {
+		queueMicrotask(propagate_changes);
+		update_set = new Set();
+	}
+}
+
+export class WaitSet extends Set {
+	aquire() {
+		this.add(context);
+	}
+	enqueue() {
+		queue_propagation();
+		update_set.add(...this);
+	}
+}
+
+export function d(initial_value, { did_change = (a,b) => a !== b } = {}) {
+	let value = initial_value;
+	const ws = new WaitSet();
+	function get() {
+		ws.aquire();
+		return value;
+	}
+	function set(new_value) {
+		if (did_change(value, new_value)) {
+			value = new_value;
+			ws.enqueue();
+		}
+	}
+	return [get, set];
+}
