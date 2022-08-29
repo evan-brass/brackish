6a57ff58c3b5e6f66e96453815d00151aa586bd4
Evan Brass <brassevan@gmail.com>
2022-08-29 16:16:01 -0700

    Cleanup

--- src/templating/expressions.mjs
+++ src/templating/expressions.mjs
@@ -1,16 +1,10 @@
 // Event Handling
 export function on(event, handler, options = {}) {
 	return node => {
-		if (!node?.addEventListener) throw new Error("on only works on EventTargets");
-		node.addEventListener(event, handler, options);
+		if ('addEventListener' in node) {
+			node.addEventListener(event, handler, options);
+		} else {
+			throw new Error("on only works with EventTargets");
+		}
 	};
-}
-
-export function text(func) {
-	return node => {
-		if (!(node instanceof Comment)) throw new Error("text only works in a node position");
-		const text = new Text();
-		node.replaceWith(text);
-		func(text);
-	}
 }
--- src/templating/index.mjs
+++
@@ -1,5 +0,0 @@
-export { default as html } from './html.mjs';
-export { mount, save } from './mount.mjs';
-export {
-	on
-} from './expressions.mjs';
