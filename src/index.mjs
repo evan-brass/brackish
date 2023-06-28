03c966b569e54b033cbaec6cc7fd9ba698f210c0
Evan Brass <brassevan@gmail.com>
2023-06-28 10:12:47 -0700

    Export apply_expression

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,7 @@
 export * from './templating/html.mjs';
 export * from './templating/mount.mjs';
 export * from './templating/expressions.mjs';
+export * from './templating/apply-expression.mjs';
 
 // Reactivity
 export * from './reactivity/signal.mjs';
